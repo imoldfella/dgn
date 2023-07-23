26064358e50fadc9300d3095123ef01dbbea57dc
jim <jimh@datagrove.com>
2023-07-23 19:34:14 -0400

    a

--- packages/ui/src/code_mirror/codemirror.tsx
+++ packages/ui/src/code_mirror/codemirror.tsx
@@ -1,2 +1,10 @@
-import { mountCodemirror } from '@'
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik'
+import { mountDgcm } from 'dgcm'
 
+export const CodeEditor = component$<{ language: string }>(() => {
+    const el = useSignal<HTMLElement>()
+    useVisibleTask$(()=>{
+        mountDgcm(el.value!)
+    })
+    return <div ref={el} />
+})
