b10073fa4029544b0d1635cdd3b31ffc033769c2
Jim Hurd <jimh@datagrove.com>
2023-08-15 10:06:56 -0400

    a

--- go.mod
+++ go.mod
@@ -4,12 +4,22 @@ go 1.20
 
 require github.com/gliderlabs/ssh v0.3.5
 
-require github.com/kr/fs v0.1.0 // indirect
+require (
+	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0 // indirect
+	github.com/invopop/jsonschema v0.7.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+)
 
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/go-co-op/gocron v1.31.1
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/sftp v1.13.6
+	github.com/xeipuuv/gojsonschema v1.2.0
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/term v0.11.0
--- ssh/main.go
+++ ssh/main.go
@@ -7,37 +7,131 @@ import (
 	"os"
 	"os/signal"
 	"path"
-	"sync/atomic"
-	"time"
+	"strings"
 
 	"github.com/gliderlabs/ssh"
 	"github.com/go-co-op/gocron"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/pkg/sftp"
+	"github.com/xeipuuv/gojsonschema"
 	"golang.org/x/term"
 )
 
+// the arg can correspond to additional arguments on the command line that can be used to override and spread to the task. Can we have a generic Partial in golang like typescript?
+
+var taskmap map[string]func(arg string) string
+
+var running *gocron.Scheduler
+var home string = "."
+
+// every task will have a task list "run": [{ "at": "7:30am" }]
+type Task struct {
+	Every int    `json:"every,omitempty"`
+	Unit  string `json:"unit,omitempty"`
+	At    string `json:"at,omitempty"`
+}
+
+type Schedule struct {
+	Run []Task `json:"run,omitempty"`
+}
+type Sample struct {
+	Schedule
+}
+
 func main() {
+	// each task corresponds to a json file in the task directory.
+	if len(os.Args) > 1 {
+		home = os.Args[1]
+	}
+
+	f := Load()
+	if len(f) > 0 {
+		for _, e := range f {
+			log.Println(e)
+		}
+		return
+	}
+
 	SshStart()
 }
 
-var version int64
-
-func RunChrone() {
-	s := gocron.NewScheduler(time.UTC)
-	for {
-		v := atomic.LoadInt64(&version)
-		for version == v {
-
+// return list of errors
+func Load() []string {
+	files, e := os.ReadDir(path.Join(home, "schema"))
+	if e != nil {
+		return []string{e.Error()}
+	}
+	for _, f := range files {
+		if !f.IsDir() {
+			schemaLoader := gojsonschema.NewReferenceLoader(path.Join(home, f.Name()))
+			schema[f.Name()] = schemaLoader
 		}
 	}
+	tasks, e := os.ReadDir(path.Join(home, "task"))
+	if e != nil {
+		return nil, e
+	}
+	for _, f := range tasks {
+		if !f.IsDir() {
+			documentLoader := gojsonschema.NewReferenceLoader(path.Join(home, f.Name()))
+			result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+			if err != nil {
+				return f.Name(), err
+			}
+		}
+	}
+	return nil, nil
 }
 
-// load is a command that replaces the current schedule with a new one.
-// it fails, leaving the current schedule in place, if the new schedule is invalid.
-func LoadChron() {
+/*
+	var tl TaskList
+	// load the schedule
+	err := json.Unmarshal(config, &tl)
+	if err != nil {
+		return err
+	}
 
+	s := gocron.NewScheduler(time.UTC)
+	for _, t := range tl.Tasks {
+		if taskmap[t.Do] == nil {
+			return fmt.Errorf("unknown task %s", t.Do)
+		}
+		ev := t.Every
+		if ev < 1 {
+			ev = 1
+		}
+		at := t.At
+		if at == "" {
+			at = "00:00"
+		}
+		unit := t.Unit
+		if unit == "" {
+			unit = "day"
+		}
+
+		switch unit {
+		case "day":
+			s.Every(ev).Day().At(at).Do(taskmap[t.Do])
+		case "second":
+			s.Every(ev).Seconds().Do(taskmap[t.Do])
+		}
+	}
+
+	// everything parses.
+	s.StartAsync()
+
+	if running != nil {
+		running.Stop()
+	}
+	running = s
+
+	// start running a new schedule; old one will finish then stop.
+	s.StartAsync()
+	return nil
 }
+*/
+
+// each task can take a configuration file as an argument and returns a string.
 
 // ideally this will give us access to both the r2 html store and to the log store.
 // Note that in either case this is more of a proxy. There's no way for an sftp interface on the server to function because it wouldn't/shouldn't have the encryption key. so this only some client/proxy magic?
@@ -46,27 +140,44 @@ type Config struct {
 	Key  string
 }
 
-func Server1(sx *Config) {
-	go func() {
-		ssh.Handle(func(s ssh.Session) {
-			s.User()
-			if len(s.Command()) > 0 {
-				for _, c := range s.Command() {
-					io.WriteString(s, "I heard "+c+"\n")
-				}
+func CommandLine(sx *Config) {
+	command := func(t io.Writer, cmd string) {
+		s := strings.Fields(cmd)
+		task := taskmap[s[0]]
+		if task == nil {
+			fmt.Fprintf(t, "unknown command %s\n", s[0])
+			return
+		}
+		var b []byte = nil
+		if len(s) > 1 {
+			bx, e := os.ReadFile(s[1])
+			if e != nil {
+				fmt.Fprintf(t, "error reading %s: %s\n", s[1], e)
 				return
 			}
+			b = bx
+		}
+		response := task(b)
+		io.WriteString(t, response+"\n")
+	}
+
+	go func() {
+		ssh.Handle(func(s ssh.Session) {
 			t := term.NewTerminal(s, "> ")
+
+			s.User()
+			if len(s.Command()) > 0 {
+				o := strings.Join(s.Command(), " ")
+				command(t, o)
+				return
+			}
+
 			for {
 				line, err := t.ReadLine()
 				if err != nil {
 					break
 				}
-				response := "I heard " + line
-				log.Println(line)
-				if response != "" {
-					t.Write(append([]byte(response), '\n'))
-				}
+				command(t, line)
 			}
 			log.Println("terminal closed")
 		})
@@ -85,7 +196,7 @@ func Server1(sx *Config) {
 			kf(&ssh_server)
 			log.Fatal(ssh_server.ListenAndServe())
 		} else {
-
+			// without sftp
 			log.Fatal(ssh.ListenAndServe(sx.Sftp, nil,
 				ssh.HostKeyFile(sx.Key),
 				ssh.PublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
@@ -109,7 +220,7 @@ func SshStart() {
 		// id_rsa of the account running the service is convenient
 		Key: path.Join(dir, ".ssh/id_rsa"),
 	}
-	Server1(sx)
+	CommandLine(sx)
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 	<-interrupt
@@ -142,3 +253,30 @@ func SftpHandlerx(sess ssh.Session) {
 		fmt.Println("sftp server completed with error:", err)
 	}
 }
+
+/*
+	"daily": func(arg []byte) string {
+		var v struct {
+			Version int64 `json:"version,omitempty"`
+		}
+		if arg == nil {
+			v.Version = 1
+		} else {
+			e := json.Unmarshal(arg, &v)
+			if e != nil {
+				return e.Error()
+			}
+		}
+
+		o, _ := json.Marshal(&v)
+		return string(o)
+	},
+	"restart": func(arg []byte) string {
+		e := Load(arg)
+		if e != nil {
+			return e.Error()
+		} else {
+			return "ok"
+		}
+	},
+*/
