5c38a89717ead41268155a87fe320a386f609d0d
Jim Hurd <jimh@datagrove.com>
2023-08-23 18:21:46 -0400

    a

--- dgdb/ui/vite.config.ts
+++ dgdb/ui/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mkcert from'vite-plugin-mkcert'
 
 export default defineConfig(() => {
   return {
@@ -12,8 +13,10 @@ export default defineConfig(() => {
         ws: true
       }, 
     },
-
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    server: {
+      https: true,
+    },
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),mkcert()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
